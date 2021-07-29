878290f399c8b371f98d2d6209e361549284ca24
Praveen Kumar <praveenscience@gmail.com>
2021-07-29 07:42:07 +0100

    We saw a lot of React concepts.
    * Class Components.
    * States and Props.
    * Rendering Data.
    * HTML in JS.
    * Created two components.
    * Imported a child component.
    * Used state to save local data.
    * Used props to pass data from one component to another.
    * Debugging using Pre & JSON.stringify().
    * Event Handlers for handling click events on a button.
    * Preventing default action of reloading page when button clicked.
    * Static vs. Dynamic Props.
    * Setting the state in the right way.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import Child from "./Child";
 
 class App extends Component {
+  state = {
+    Name: "Edusera"
+  };
+  handleClick = e => {
+    e.preventDefault();
+    this.setState({ Name: "Devlina" });
+  };
   render() {
     return (
       <div className="App">
         <h1>Learn React</h1>
-        <p>You are the best!</p>
+        <p>Hey {this.state.Name}, you are the best!</p>
+        <button onClick={this.handleClick}>Change Name</button>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        <Child Name="Nirankar" MainName={this.state.Name} />
       </div>
     );
   }
