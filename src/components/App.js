5b753fcddc37c7dfbed9a8bcbc15f4b00c6f3445
Praveen Kumar <praveenscience@gmail.com>
2021-07-29 07:24:30 +0100

    Reorganise the boilerplate and cleanup.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Learn React</h1>
+        <p>You are the best!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
