4c740a7d055b8e384e6ca3eda5075ec0b6c2c175
Praveen Kumar <praveenscience@gmail.com>
2021-07-30 07:08:59 +0100

    Added dark support for header.

--- src/components/Bootstrap/Header.js
+++ src/components/Bootstrap/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Header = ({ children, className, containerClass }) => {
+const Header = ({ children, className, containerClass, dark }) => {
+  dark = dark ? "dark" : "light";
   return (
     <nav
       className={
-        "navbar navbar-light bg-light" + (className ? " " + className : "")
+        `navbar navbar-${dark} bg-${dark}` + (className ? " " + className : "")
       }
     >
       <div
