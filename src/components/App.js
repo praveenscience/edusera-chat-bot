1b43587b1457d0062368453249508ec8b088afc7
Praveen Kumar <praveenscience@gmail.com>
2021-07-30 07:06:41 +0100

    Included Bootstrap Header.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import Header from "./Bootstrap/Header";
 
 class App extends Component {
   state = {};
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Header>ChatBot Without AI</Header>
+      </div>
+    );
   }
 }
 
