016384d1bc0a202019f6aa9c9f4689c4c5160691
Praveen Kumar <praveenscience@gmail.com>
2021-07-30 07:35:29 +0100

    Toggle Chat Open & Close.

--- src/components/ChatBot/ChatBot.js
+++ src/components/ChatBot/ChatBot.js
@@ -4,6 +4,7 @@ import GuestImg from "../../assets/guest.png";
 
 class ChatBot extends Component {
   state = {
+    Open: false,
     Messages: [
       {
         Text: "Hey there! I am a ChatBot! Type start!",
@@ -15,25 +16,32 @@ class ChatBot extends Component {
       }
     ]
   };
+  toggleChatBox = () => {
+    this.setState({
+      Open: !this.state.Open
+    });
+  };
   render() {
     return (
       <div className="ChatBot-Wrapper">
-        <div className="ChatBot-Messages">
-          <ul>
-            {this.state.Messages.map((msg, key) => (
-              <li key={key} className={msg.Bot ? "ChatBot" : "Guest"}>
-                <img
-                  src={msg.Bot ? ChatBotImg : GuestImg}
-                  alt={msg.Bot ? "ChatBot" : "Guest"}
-                />
-                <p>{msg.Text}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <form className="ChatBot-Input"></form>
+        {this.state.Open && (
+          <div className="ChatBot-Messages">
+            <ul>
+              {this.state.Messages.map((msg, key) => (
+                <li key={key} className={msg.Bot ? "ChatBot" : "Guest"}>
+                  <img
+                    src={msg.Bot ? ChatBotImg : GuestImg}
+                    alt={msg.Bot ? "ChatBot" : "Guest"}
+                  />
+                  <p>{msg.Text}</p>
+                </li>
+              ))}
+            </ul>
+            <form className="ChatBot-Input"></form>
+          </div>
+        )}
         <div className="ChatBot-Trigger">
-          <img src={ChatBotImg} alt="Open Chat" />
+          <img src={ChatBotImg} alt="Open Chat" onClick={this.toggleChatBox} />
         </div>
       </div>
     );
