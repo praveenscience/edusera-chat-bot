ad1e0dc8f977f940f4015ea1bca0a8baf20ebaed
Praveen Kumar <praveenscience@gmail.com>
2021-07-30 06:47:34 +0100

    Cleanup.

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,9 @@
 import React, { Component } from "react";
-import Child from "./Child";
 
 class App extends Component {
-  state = {
-    Name: "Edusera"
-  };
-  handleClick = e => {
-    e.preventDefault();
-    this.setState({ Name: "Devlina" });
-  };
+  state = {};
   render() {
-    return (
-      <div className="App">
-        <h1>Learn React</h1>
-        <p>Hey {this.state.Name}, you are the best!</p>
-        <button onClick={this.handleClick}>Change Name</button>
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
-        <Child Name="Nirankar" MainName={this.state.Name} handleClick={this.handleClick} />
-      </div>
-    );
+    return <div className="App"></div>;
   }
 }
 
