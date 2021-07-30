15f33d5806cc0d79de0aa70c1f366085f1e1ecde
Praveen Kumar <praveenscience@gmail.com>
2021-07-30 07:11:13 +0100

    Added a dark header.

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header containerClass="justify-content-center">
+        <Header containerClass="justify-content-center" dark={true}>
           ChatBot Without AI
         </Header>
       </div>
